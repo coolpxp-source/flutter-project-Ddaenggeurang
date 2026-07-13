39dccb08e4e6e748a63191ef652feecbbb81ed2b
Hyejin AHN <coolpxp@gmail.com>
2026-07-13 18:30:27 +0900

    Revert "feat: 커뮤니티 도메인 완성 (홈/랭킹/또래비교/저축공유 + 게시글·댓글 CRUD)"

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-        isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
@@ -36,10 +35,6 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
-}
-
-dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 kotlin {
