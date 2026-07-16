b2cef2cb2a45c5e1543371d9100c3d11f81f814c
Ahn Hyejin <coolpxp@gmail.com>
2026-07-16 17:04:29 +0900

    오류수정을 위한 push

--- lib/screens/expense/expense_input_screen.dart
+++ lib/screens/expense/expense_input_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import '../../models/expense_model.dart';
+import '../../services/category_service.dart';
 import '../../services/expense_service.dart';
 import '../../utils/currency_formatter.dart';
 
@@ -18,26 +19,13 @@ class ExpenseInputScreen extends StatefulWidget {
   }
 }
 
-<<<<<<< HEAD
+
 class _ExpenseInputScreenState extends State<ExpenseInputScreen> {
   final _expenseService = ExpenseService();
-  final _categoryService = categoryService();
+  final _categoryService = CategoryService();
   final _amountController = TextEditingController(text: '0');
   final _placeController = TextEditingController();
   final _memoController = TextEditingController();
-=======
-class _ExpenseInputScreenState
-    extends State<ExpenseInputScreen> {
-  final TextEditingController _amountController =
-  TextEditingController();
-
-  final TextEditingController _memoController =
-  TextEditingController();
-
-  final ExpenseService _expenseService =
-  ExpenseService();
->>>>>>> e6e0e506bf55ff6b93c41c4a8468799914287a69
-
   DateTime _selectedDate = DateTime.now();
 
   /// 카테고리 조회 상태
